a166e5731a39c6927d65ffa071413b82296300f2
itpohgero <itpohgero@gmail.com>
2022-08-21 22:26:24 +0700

    add blogs

--- models/blog.go
+++ models/blog.go
@@ -1,11 +1,13 @@
 package models
 
+import "time"
+
 type Blog struct {
-	Id        uint   `json:"id" validate:"required"`
-	UserID    uint   `json:"user_id" validate:"required"`
-	Slug      string `json:"slug" validate:"required"`
-	Title     string `json:"title" validate:"required"`
-	Body      string `json:"body" validate:"required"`
-	CreatedAt string `json:"created_at" validate:"required"`
-	UpdatedAt string `json:"updatedAt" validate:"required"`
+	Id        uint      `json:"id" validate:"required"`
+	UserID    uint      `json:"user_id" validate:"required"`
+	Slug      string    `json:"slug" validate:"required"`
+	Title     string    `json:"title" validate:"required"`
+	Body      string    `json:"body" validate:"required"`
+	CreatedAt time.Time `gorm:"autoCreateTime:true"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:true"`
 }
--- models/user.go
+++ models/user.go
@@ -1,8 +1,12 @@
 package models
 
+import "time"
+
 type User struct {
-	Id       uint   `json:"id" validate:"required"`
-	Name     string `json:"name" validate:"required"`
-	Email    string `json:"email" gorm:"unique" validate:"required,email"`
-	Password []byte `json:"-" validate:"required"`
+	Id        uint      `json:"id" validate:"required"`
+	Name      string    `json:"name" validate:"required"`
+	Email     string    `json:"email" gorm:"unique" validate:"required,email"`
+	Password  []byte    `json:"-" validate:"required"`
+	CreatedAt time.Time `gorm:"autoCreateTime:true"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime:true"`
 }
