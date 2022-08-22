9aa54a9be6b56be044e826c37b0353d6fd2bc6a1
itpohgero <itpohgero@gmail.com>
2022-08-22 10:42:57 +0700

    configurasi database postgres

--- database/connection.go
+++ database/connection.go
@@ -4,6 +4,7 @@ import (
 	"Golang-fiber/models"
 	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
 	"os"
@@ -17,16 +18,27 @@ func Connect() {
 		log.Fatal("Error loading .env file")
 	}
 
-	dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT") + ")/" + os.Getenv("DB_NAME")
-	connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
-	if err != nil {
-		panic("failed to connect database")
+	if os.Getenv("DB_TYPE") == "mysql" {
+		dsn := os.Getenv("DB_USER") + ":" + os.Getenv("DB_PASSWORD") + "@tcp(" + os.Getenv("DB_HOST") + ":" + os.Getenv("DB_PORT_MYSQL") + ")/" + os.Getenv("DB_NAME")
+		connection, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+		if err != nil {
+			panic("failed to connect mysql database")
+		}
+		DB = connection
+		err = connection.AutoMigrate(&models.User{}, &models.Blog{})
+		if err != nil {
+			return
+		}
 	}
 
-	DB = connection
+	if os.Getenv("DB_TYPE") == "postgres" {
+		dsn := "host=" + os.Getenv("DB_HOST") + " user=" + os.Getenv("DB_HOST") + " password=" + os.Getenv("DB_PASS") + " dbname=" + os.Getenv("DB_NAME") + " port=" + os.Getenv("DB_PORT_POSTGRES") + " sslmode=disable TimeZone=Asia/Jakarta"
+		connection, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	err = connection.AutoMigrate(&models.User{}, &models.Blog{})
-	if err != nil {
-		return
+		DB = connection
+		err = connection.AutoMigrate(&models.User{}, &models.Blog{})
+		if err != nil {
+			return
+		}
 	}
 }
